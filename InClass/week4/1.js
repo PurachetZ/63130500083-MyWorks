498f2cbe79263e3f2217fa9ad9c8139556cb6d04
Purachet Zheng <poorachad2545@gmail.com>
2021-10-02 22:44:17 +0700

    week 4 inclass create

---
+++ InClass/week4/1.js
@@ -0,0 +1,6 @@
+const value = {id:1, name:"test"}
+const student = {id:1, name:"Somchai"}
+//student = value; เปลี่ยน address ไม่ได้
+console.log(student);
+student.email = 'test@test';
+console.log(student);
