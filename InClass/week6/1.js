1ef9c932a5e90b1995b40d302b5981c032148149
Purachet Zheng <poorachad2545@gmail.com>
2021-10-03 07:32:58 +0700

    update

---
+++ InClass/week6/1.js
@@ -0,0 +1,23 @@
+let num1 = 10;
+let num2 = 20;
+num2 = num1;
+console.log(num1 == num2);
+num2 = 55;
+console.log(num1)
+
+let per1 = { id:1, name: 'P'};
+let per2 = { id:1, name: 'P'};
+
+per2 = per1;
+console.log(per1 == per2);
+
+let arr1 = [0, 2, 4, 6];
+let arr2 = [0, 2, 4, 6];
+console.log(arr1 === arr2);
+arr2 = arr1;
+arr2[1] = 5;
+console.log(arr1[1]);
+console.log(arr1);
+console.log(arr2);
+console.log(arr1 === arr2);
+
