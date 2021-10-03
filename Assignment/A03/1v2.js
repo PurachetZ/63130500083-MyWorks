dbb5017a3d80f462e1b4c67ed9187213f382073a
Purachet Zheng <poorachad2545@gmail.com>
2021-10-03 23:59:20 +0700

    A03_1 foreach 2 line Ver.2

--- Assignment/A03/1v2.js
+++ Assignment/A03/1v2.js
@@ -13,11 +13,14 @@ function changeCalculator(order, pay) {
     let changeResult = {};
     for(let bc of billCoin){
         changeResult[Object.keys(bc)] = Math.floor(change / bc[Object.keys(bc)]);
-        change -= (bc[Object.keys(bc)]) * Math.floor(change / bc[Object.keys(bc)]); 
+        change %= (bc[Object.keys(bc)]); 
+        // changeResult[Object.keys(bc)] = Math.floor(change / bc[Object.keys(bc)]);
+        // change -= (bc[Object.keys(bc)]) * Math.floor(change / bc[Object.keys(bc)]); 
     }
     return changeResult;
   }
 console.log(changeCalculator(150, 1000));
+console.log(changeCalculator(199, 1000));
 console.log(changeCalculator(1500, 2000));
 console.log(changeCalculator(3950, 4000));
 console.log(changeCalculator(501, 1000));
