93e2ad25c9f76e156c7c854f162502bbbfa9c3c5
Purachet Zheng <poorachad2545@gmail.com>
2021-11-07 22:36:08 +0700

    update search class

--- Assignment/product/product-search.js
+++ Assignment/product/product-search.js
@@ -2,12 +2,20 @@ import { products } from './product.js';
 const divProductsEle = document.querySelector('#products');
 
 const divSearchEle = document.querySelector('#search');
-divSearchEle.setAttribute('class', 'container row mt-5 mx-auto');
+divSearchEle.setAttribute('class', 'container row my-5 mx-auto');
 
 const sIconEle = document.createElement('i');
 sIconEle.setAttribute('class', 'col-auto fa fa-search fa-lg');
 sIconEle.setAttribute('style', `padding-top: 10px;`);
-sIconEle.onclick = function() {SearchIconBtn()};
+sIconEle.onclick = function() {
+if(sFormEle.style.visibility == 'hidden'){
+    sFormEle.style.visibility = 'visible';
+    sButtonEle.style.visibility = 'visible';
+}
+else{
+    sFormEle.style.visibility = 'hidden';
+    sButtonEle.style.visibility = 'hidden';
+}};
 divSearchEle.appendChild(sIconEle);
 
 const sFormEle = document.createElement('input');
@@ -37,78 +45,71 @@ searchBtn.onclick = function() {
     createProductByName(shoeName.value);
 }
 
-function SearchIconBtn(){
-    if(sFormEle.style.visibility == 'hidden'){
-        sFormEle.style.visibility = 'visible';
-        sButtonEle.style.visibility = 'visible';
+
+let createProductByName = (search) => products.forEach(product => {
+    if(product.productName.toLowerCase().match(search.toLowerCase())){
+        const divProductEle = document.createElement('div');
+        divProductEle.setAttribute('id', product.productId);
+        divProductEle.setAttribute('class', 'col-4 card border-0 mb-4 mx-2 p-0');
+        divProductEle.setAttribute('style', 'cursor: pointer; width: 25em;')
+
+        const pProductImgEle = document.createElement('img');
+        pProductImgEle.setAttribute('src', product.img);
+        pProductImgEle.setAttribute('class', 'card-img-top');
+        divProductEle.appendChild(pProductImgEle);
+      
+        const pProductIdEle = document.createElement('h5');
+        pProductIdEle.textContent = product.productId;
+        pProductIdEle.setAttribute('class', 'card-title text-center');
+        // pProductIdEle.setAttribute('style', 'margin: -40px 0px 30px;');
+        pProductIdEle.setAttribute('style', 'margin: -380px 0px 370px;');
+        divProductEle.appendChild(pProductIdEle);
+      
+        const pProductNameEle = document.createElement('h5');
+        pProductNameEle.textContent = product.productName;
+        pProductNameEle.setAttribute('class', 'card-title');
+        divProductEle.appendChild(pProductNameEle);
+      
+        const pProductPriceEle = document.createElement('h6');
+        pProductPriceEle.textContent = product.productPrice + ' Baht';
+        pProductPriceEle.setAttribute('class', 'card-text text-secondary');
+        divProductEle.appendChild(pProductPriceEle);
+        
+        const pProductStockEle = document.createElement('h6');
+        pProductStockEle.textContent = 'Available : ' + product.productStock;
+        pProductStockEle.setAttribute('class', 'card-text text-secondary');
+        divProductEle.appendChild(pProductStockEle);
+      
+        // const pProductBuyEle = document.createElement('button');
+        // pProductBuyEle.setAttribute('type', 'button');
+        // pProductBuyEle.setAttribute('class', 'btn btn-primary');
+        // pProductBuyEle.textContent = 'Buy Now';
+        // pProductBuyEle.onclick = function() {}
+        // divProductEle.appendChild(pProductBuyEle);
+      
+        divProductsEle.appendChild(divProductEle);
     }
-    else{
-        sFormEle.style.visibility = 'hidden';
-        sButtonEle.style.visibility = 'hidden';
-    }
-}
+})
 
-
-
-
-function createProductByName(search){
-    for (let product of products) {
-        if(product.productName.toLowerCase().match(search.toLowerCase())){
-            const divProductEle = document.createElement('div');
-            divProductEle.setAttribute('id', product.productId);
-            divProductEle.setAttribute('class', 'col-4 card border-0 mb-4 mx-2 p-0');
-            divProductEle.setAttribute('style', 'cursor: pointer; width: 25em;')
-
-            const pProductImgEle = document.createElement('img');
-            pProductImgEle.setAttribute('src', product.img);
-            pProductImgEle.setAttribute('class', 'card-img-top');
-            divProductEle.appendChild(pProductImgEle);
-          
-            const pProductIdEle = document.createElement('h5');
-            pProductIdEle.textContent = product.productId;
-            pProductIdEle.setAttribute('class', 'card-title text-center');
-            // pProductIdEle.setAttribute('style', 'margin: -40px 0px 30px;');
-            pProductIdEle.setAttribute('style', 'margin: -380px 0px 370px;');
-            divProductEle.appendChild(pProductIdEle);
-          
-            const pProductNameEle = document.createElement('h5');
-            pProductNameEle.textContent = product.productName;
-            pProductNameEle.setAttribute('class', 'card-title');
-            divProductEle.appendChild(pProductNameEle);
-          
-            const pProductPriceEle = document.createElement('h6');
-            pProductPriceEle.textContent = product.productPrice + ' Baht';
-            pProductPriceEle.setAttribute('class', 'card-text text-secondary');
-            divProductEle.appendChild(pProductPriceEle);
-            
-            const pProductStockEle = document.createElement('h6');
-            pProductStockEle.textContent = 'Available : ' + product.productStock;
-            pProductStockEle.setAttribute('class', 'card-text text-secondary');
-            divProductEle.appendChild(pProductStockEle);
-          
-            // const pProductBuyEle = document.createElement('button');
-            // pProductBuyEle.setAttribute('type', 'button');
-            // pProductBuyEle.setAttribute('class', 'btn btn-primary');
-            // pProductBuyEle.textContent = 'Buy Now';
-            // pProductBuyEle.onclick = function() {}
-            // divProductEle.appendChild(pProductBuyEle);
-          
-            divProductsEle.appendChild(divProductEle);
-        }
-    }
-}
 
 // function matchName(pName = '', sName= ''){
 //     return pName.toLowerCase().match(sName.toLowerCase());
 // }
 
-function removeProductList(){
+let removeProductList = () => {
     let size = divProductsEle.childNodes.length;
     for(let i = 0; i < size; i++){
-      divProductsEle.removeChild(divProductsEle.childNodes[0]);
+        divProductsEle.removeChild(divProductsEle.childNodes[0]);
     }
-    // for(let product of divProductsEle.childNodes){
-    //     console.log(product)
-    //     divProductsEle.removeChild(product);
-    // }
 }
+
+// function removeProductList(){
+//     let size = divProductsEle.childNodes.length;
+//     for(let i = 0; i < size; i++){
+//       divProductsEle.removeChild(divProductsEle.childNodes[0]);
+//     }
+//     // for(let product of divProductsEle.childNodes){
+//     //     console.log(product)
+//     //     divProductsEle.removeChild(product);
+//     // }
+// }
