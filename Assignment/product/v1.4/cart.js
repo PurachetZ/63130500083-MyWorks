e710bc1dc854206f5bf8839c5b7ea49dae58b071
Purachet Zheng <poorachad2545@gmail.com>
2021-11-10 00:01:57 +0700

    move all version of all product to folder product

---
+++ Assignment/product/v1.4/cart.js
@@ -0,0 +1,35 @@
+const countCartEle = document.getElementById('cart-count');
+const deleteCartBtnEle = document.getElementById('delete-cart');
+deleteCartBtnEle.addEventListener("click",removeCart);
+export let cart = [];
+
+export function addCart(e){
+    let x = cart.findIndex(cart => cart.productId == e.target.parentNode.id);
+    // let x = cart.filter(cart => cart.productId == e.target.parentNode.id);
+    if(x == -1){
+        alert(`${e.target.parentNode.id} added in your cart`);
+        cart.push({
+            productId: e.target.parentNode.id,
+            // productName: product.productName,
+            // productPrice: product.productPrice,
+            qty: 1
+        })
+    }
+    else{
+        cart[cart.findIndex(ele => ele.productId === e.target.parentNode.id)].qty++;
+    }
+    countCartEle.textContent = countCart();
+}
+
+export function removeCart() {
+    cart = [];
+    countCartEle.textContent = countCart();
+}
+
+export function countCart(){
+    let sum = 0;
+    cart.forEach(product => {sum += product.qty;})
+    return sum;
+}
+
+
